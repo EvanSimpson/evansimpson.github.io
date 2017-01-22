79eb114d3885d5011eae21675f52ffc2fe32e31f
Evan Simpson <me@evansimpson.com>
2017-01-22 13:27:05 -0500

    Lint index file

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import { Link } from 'react-router'
-import { prefixLink } from 'gatsby-helpers'
-import Helmet from 'react-helmet'
-import { config } from 'config'
+import React from 'react';
+import { Link } from 'react-router';
+import { prefixLink } from 'gatsby-helpers';
+import Helmet from 'react-helmet';
+import { config } from 'config';
 
 import styles from '../css/home.module.css';
 
